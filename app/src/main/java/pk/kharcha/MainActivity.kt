2fce07431801f0da660dda99b8477aadd5477323
Zeeshan-Ali <zeeshanali.projectmarker@myvteams.com>
2026-08-25 16:30:52 +0500

    fixed apk issue

--- app/src/main/java/pk/kharcha/MainActivity.kt
+++ app/src/main/java/pk/kharcha/MainActivity.kt
@@ -4,6 +4,7 @@ import android.Manifest
 import android.app.Application
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.runtime.*
--- app/src/main/java/pk/kharcha/ui/HomeScreen.kt
+++ app/src/main/java/pk/kharcha/ui/HomeScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import pk.kharcha.data.*
